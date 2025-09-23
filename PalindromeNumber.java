27a256125d5b9e5f09cc86dc0f0e18974126f828
dskarthi2005 <dskarthi2005@gmail.com>
2025-09-23 18:22:02 +0530

    Palindrome problem

---
+++ PalindromeNumber.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int temp = n, rev = 0;
+
+        while (temp > 0) {
+            rev = rev * 10 + (temp % 10);
+            temp /= 10;
+        }
+
+        if (rev == n)
+            System.out.println("Palindrome Number");
+        else
+            System.out.println("Not Palindrome Number");
+    }
+}
